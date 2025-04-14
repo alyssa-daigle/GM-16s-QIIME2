d2a85e3c305e6cc0c04408d302dfdd4566289c34
alyssa-daigle <143747222+alyssa-daigle@users.noreply.github.com>
2025-04-14 14:03:41 -0400

    Update 03_taxize.R

--- 03_taxize.R
+++ 03_taxize.R
@@ -3,12 +3,11 @@ library(taxize)
 library(purrr)
 library(dplyr)
 
-#loading taxonomy data
+#loading and filtering taxonomy data
 taxonomy <- read.table("~/Desktop/taxize/taxonomy.tsv", 
                        header = TRUE, 
                        sep = "\t", 
                        stringsAsFactors = FALSE)
-
 
 taxonomy <- taxonomy |> 
     separate(Taxon, into = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus"), 
@@ -25,24 +24,23 @@ taxonomy$Genus <- sub("^.*?__(.*)", "\\1", taxonomy$Genus)
 #filtering by family
 unique_families <- unique(taxonomy$Family)
 
+#performs this slightly slower so that R memory doesn't run out
 taxize::taxize_options(ncbi_sleep = 0.5)
 
+#filter is set to only grab taxa IDs if they fall into bacteria, phyta, and archaetoa (no eukaryotes)
 filter = "^(bacteria.*|.*phyta$|.*archaeota$)"
+
+#actually getting taxa IDs
 family_ncbi_ids <- get_uid(unique_families, 
                            rank_query = "family",
                            division_filter = filter)
 
-
+#saving as a df 
 family_taxa_df <- tibble(Family = unique_families, Taxid = family_ncbi_ids)
 
-
+#sees which families did not get assigned a taxa ID
 missing_family_df <- family_taxa_df |> 
 	filter(is.na(Taxid))
 
-
 #write out as csv
 write.csv(family_taxa_df, "~/Desktop/family_taxa.csv", row.names = FALSE)
-
-
-
-
